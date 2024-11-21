3bd5553f207f3c25807e6ad4d3fd94e73f62403c
Dorian <dorian.boussakine@gmail.com>
2024-11-21 14:15:24 +0200

    Käyttäjä ja Tuote Create ja Insert koodit ovat implementoitu

--- main_app.py
+++ main_app.py
@@ -12,7 +12,7 @@ def execute_sql_file(connection, file_path):
 
 
 def main():
-    # Database connection details
+    # Tietokantayhteyden tiedot
     db_config = {
         "dbname": "kit_it_db",
         "user": "postgres",
@@ -21,16 +21,16 @@ def main():
         "port": 5432
     }
 
-    # Connect to the PostgreSQL database
+    # Yhdistä PostgreSQL-tietokantaan
     try:
         conn = psycopg2.connect(**db_config)
         print("Tietokanta yhdistetty onnistuneesti.")
 
-        # # Execute SQL files
-        # execute_sql_file(conn, 'sql/create_tables.sql')
-        # execute_sql_file(conn, 'sql/insert_data.sql')
+        # Suorita SQL-tiedostoja
+        execute_sql_file(conn, 'sql/create_tables.sql')
+        execute_sql_file(conn, 'sql/insert_data.sql')
 
-        # print("SQL-skriptit suoritettu onnistuneesti.")
+        print("SQL-skriptit suoritettu onnistuneesti.")
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
