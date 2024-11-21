922069e09658ce4cae39e1a53ae97bc64e841ed5
Dorian <dorian.boussakine@gmail.com>
2024-11-21 23:52:52 +0200

    Yhteenveto (hankittu)

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,5 +1,11 @@
 -- Lisää esimerkkitiedot käyttäjätaulukkoon
-INSERT INTO users (username, email, password_hash) VALUES ('john_doe', 'john@example.com', 'hashed_password_here'), ('jane_doe', 'jane@example.com', 'hashed_password_here') ON CONFLICT (username) DO NOTHING;
+INSERT INTO users (username, email, password_hash)
+VALUES ('john_doe', 'john@example.com', 'hashed_password_here'),
+('jane_doe', 'jane@example.com', 'hashed_password_here')
+ON CONFLICT (username) DO NOTHING;
 
 -- Lisää esimerkkitiedot tuotetaulukkoon
-INSERT INTO products (user_id, name, description, price) VALUES (1, 'Laptop', 'A high-performance laptop', 999.99), (2, 'Smartphone', 'A brand-new smartphone with great features', 699.99) ON CONFLICT (id) DO NOTHING;
+INSERT INTO products (user_id, name, description, price)
+VALUES (1, 'Laptop', 'A high-performance laptop', 999.99),
+(2, 'Smartphone', 'A brand-new smartphone with great features', 699.99)
+ON CONFLICT (id) DO NOTHING;
